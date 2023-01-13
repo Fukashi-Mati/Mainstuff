122ce90faed16ed67ce467ac92b245621f80b6b6
Fukashu <57369434+Fukashi-Mati@users.noreply.github.com>
2023-01-13 10:44:06 +0100

    remake of kafka sending

--- weather.py
+++ weather.py
@@ -2,17 +2,33 @@ from json import dumps
 from kafka import KafkaProducer
 from sense_emu import SenseHat
 import time
-def getAndSendTemperature(sense):
+import sys
+def getAndSendTemperature(sens):
 
-    temperature = sense.temp
-    producer = KafkaProducer(bootstrap_servers=['192.168.0.103:9092'],
-                         value_serializer=lambda x:
-                         dumps(x).encode('utf-8'))
-    producer.send('temp2', value={'temperatura': temperature})
+    temperature = sens.temp
 
+    #producer = KafkaProducer(bootstrap_servers=['192.168.0.103:9092'],
+                     #    value_serializer=lambda x:
+                    #     dumps(x).encode('utf-8'))
+    #producer.send('temp2', value={'temperatura': temperature})
+    KAFKA_HOST = "192.168.0.103:9092"
+
+    client = KafkaClient(hosts=KAFKA_HOST)
+    topic = client.topics["temp2"]
+
+    with topic.get_sync_producer() as producer:
+        message = string(temperature)
+        encoded_message = message.encode("utf-8")
+        producer.produce(encoded_message)
 
 
 sense = SenseHat()
+num = sys.argv[1]
+num2 = 0
 while True:
-    getAndSendTemperature(sense)
-    sleep(30)
+    if num <= num2:
+        num2 = 0
+    else:
+        getAndSendTemperature(sense)
+        num2 += 1
+    sleep(1)
