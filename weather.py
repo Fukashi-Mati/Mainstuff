2bfe42ad84431bac27ad002654c8400a09ab72ea
Fukashu <57369434+Fukashi-Mati@users.noreply.github.com>
2023-01-16 20:31:33 +0100

    Even better weather script

--- weather.py
+++ weather.py
@@ -1,35 +1,30 @@
-from json import dumps
-from kafka import KafkaProducer
+from pykafka import KafkaClient, SslConfig
 from sense_emu import SenseHat
 import time
 import sys
 def getAndSendTemperature(sens):
 
-    temperature = sens.temp
-
-    #producer = KafkaProducer(bootstrap_servers=['192.168.0.103:9092'],
-                     #    value_serializer=lambda x:
-                    #     dumps(x).encode('utf-8'))
-    #producer.send('temp2', value={'temperatura': temperature})
+    temperature = 25
     KAFKA_HOST = "192.168.0.103:9092"
-
-    client = KafkaClient(hosts=KAFKA_HOST)
+    config = SslConfig(cafile='CARoot.pem',
+                       certfile='certificate.pem',
+                       keyfile='key.pem')
+    client = KafkaClient(hosts=KAFKA_HOST, ssl_config=config)
     topic = client.topics["temp2"]
 
     with topic.get_sync_producer() as producer:
-        message = string(temperature)
+        message = str(temperature)
         encoded_message = message.encode("utf-8")
         producer.produce(encoded_message)
 
 
 sense = SenseHat()
-num = sys.argv[1]
+num = int(sys.argv[1])
 num2 = 0
 while True:
     if num <= num2:
         num2 = 0
     else:
         getAndSendTemperature(sense)
-        print("wysylam")
         num2 += 1
-    sleep(1)
+    time.sleep(1)
