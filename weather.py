cbbd9ca652567d797b5808d324c21ece82ee1e98
Fukashu <57369434+Fukashi-Mati@users.noreply.github.com>
2023-01-16 21:23:27 +0100

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,22 +1,27 @@
-from pykafka import KafkaClient, SslConfig
+from kafka import KafkaProducer
 from sense_emu import SenseHat
 import time
 import sys
-def getAndSendTemperature(sens):
+def getAndSendTemperature(sense):
 
-    temperature = sens.temp
-    KAFKA_HOST = "192.168.0.103:9092"
-    config = SslConfig(cafile='CARoot.pem',
-                       certfile='certificate.pem',
-                       keyfile='key.pem',
-                       password='student123')
-    client = KafkaClient(hosts=KAFKA_HOST, ssl_config=config)
-    topic = client.topics["temp2"]
+    temperature = sense.temp
+    kafkaBrokers = '192.168.0.105:9092'
+    caRootLocation = 'CARoot.pem'
+    certLocation = 'certificate.pem'
+    keyLocation = 'key.pem'
+    topic = 'temp2'
+    password = 'student123'
 
-    with topic.get_sync_producer() as producer:
-        message = str(temperature)
-        encoded_message = message.encode("utf-8")
-        producer.produce(encoded_message)
+    producer = KafkaProducer(bootstrap_servers=kafkaBrokers,
+                             security_protocol='SSL',
+                             ssl_check_hostname=True,
+                             ssl_cafile=caRootLocation,
+                             ssl_certfile=certLocation,
+                             ssl_keyfile=keyLocation,
+                             ssl_password=password)
+
+    producer.send(topic, bytes(str(temperature), 'utf-8'))
+    producer.flush()
 
 
 sense = SenseHat()
